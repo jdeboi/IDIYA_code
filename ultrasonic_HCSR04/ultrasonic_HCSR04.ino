7ac23ae1bd336fd3b98bf260f5a2cfbd5b1a1201
jdeboi <jdeboi@gmail.com>
2016-05-17 20:36:29 -0500

    test

--- ultrasonic_HCSR04/ultrasonic_HCSR04.ino
+++ ultrasonic_HCSR04/ultrasonic_HCSR04.ino
@@ -4,40 +4,28 @@
 
 Adafruit_8x16matrix matrix = Adafruit_8x16matrix();
 
-#define trigPin 11
-#define echoPin 10
-#define buttonPin 2
-
 long startDistance = 12;
 long distance = 0;
 
 void setup() {
   Serial.begin (9600);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(buttonPin, INPUT);
+
 
   Serial.println("16x8 LED Matrix Test");
   // changed from default address of 0x70 b/c soldered
   matrix.begin(0x71);  // pass in the address
   matrix.setRotation(1);
+
+  
+  pinMode(ultrasonicPin, INPUT);
 }
 
 void loop() {
-  for(int i = 0; i < 10; i++) {
-    setDistance();
-    delayCheck(500);
-    //delay(500);
-  }
-  printDistance(); 
+  distance = int(analogRead(A0)/1023.0*5.0/1000/9.8);
   setMatrix();
   
 }
 
-void delayCheck(int delayTime) {
-  long t = millis();
-  while(millis() - t < delayTime) setMatrix();
-}
 
 void setMatrix() {
   if (digitalRead(buttonPin)) {
@@ -84,5 +72,96 @@ void printDistance() {
     Serial.println(" in");
   }
 }
+#include <Adafruit_NeoPixel.h>
+#define PIN 3
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, PIN, NEO_GRB + NEO_KHZ800);
+
+// pins for the ultrasonic sensor
+int ultrasonicPin = A0;
+int arraysize = 9; // quantity of values to find the median (sample size). Needs to be an odd number
+int rangevalue[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0};
+long pulse;
+int modE;
+
+void setup() {
+ 
+}
+
+void loop() {
+  
+ for(int i = 0; i < arraysize; i++){                    
+    //pulse = pulseIn(ultrasonicPin, HIGH);
+    pulse = analogRead(ultrasonicPin);
+    rangevalue[i] = pulse;
+    delay(10);
+  }
+  //Serial.print("Unsorted: ");
+  //printArray(rangevalue,arraysize);
+  isort(rangevalue,arraysize);
+  //Serial.print("Sorted: ");
+  //printArray(rangevalue,arraysize);
+  modE = mode(rangevalue,arraysize);
+  //Serial.print("The mode/median is: ");
+  Serial.print(modE);
+  Serial.println();
+ 
+}
+
+
+void printArray(int *a, int n) {
+  for (int i = 0; i < n; i++)
+  {
+    Serial.print(a[i], DEC);
+    Serial.print(' ');
+  }
+  Serial.println();
+}
+
+// Sorting function (Author: Bill Gentles, Nov. 12, 2010)
+void isort(int *a, int n){
+  for (int i = 1; i < n; ++i)  {
+    uint16_t j = a[i];
+    int k;
+    for (k = i - 1; (k >= 0) && (j < a[k]); k--) {
+      a[k + 1] = a[k];
+    }
+    a[k + 1] = j;
+  }
+}
+ 
+// Mode function, returning the mode or median.
+int mode(int *x,int n){
+  int i = 0;
+  int count = 0;
+  int maxCount = 0;
+  uint16_t mode = 0;
+  int bimodal;
+  int prevCount = 0;
+  while(i<(n-1)){
+    prevCount=count;
+    count=0;
+    while( x[i]==x[i+1] ) {
+      count++;
+      i++;
+    }
+    if( count > prevCount & count > maxCount) {
+      mode=x[i];
+      maxCount=count;
+      bimodal=0;
+    }
+    if( count == 0 ) {
+      i++;
+    }
+    if( count == maxCount ) {      //If the dataset has 2 or more modes.
+      bimodal=1;
+    }
+    if( mode==0 || bimodal==1 ) {  // Return the median if there is no mode.
+      mode=x[(n/2)];
+    }
+    return mode;
+  }
+}
+
+
 
 
